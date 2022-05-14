f00f65a10d49d770b1ac36815c8e97f7e6f61203
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-15 03:34:30 +0900

    #23 CRUD bootstrap FORM action of form - Hash password

--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -2,6 +2,13 @@ let createNewUser = (data) => {
     console.log("Data from service", data);
 }
 
+let hashUserPassword = (password) => {
+    // dùng promise để luôn luôn trả ra kết quả, tránh việc xử lý bất đồng bộ
+    return new Promise((resolve, reject) => {
+        
+    })
+}
+
 module.exports = {
     createNewUser: createNewUser,
 }
