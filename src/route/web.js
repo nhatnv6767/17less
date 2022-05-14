dfbaa5b1fb395ecef112f2cc58dc45ad5790b6f9
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-14 15:04:05 +0900

    #18 - 2

--- src/route/web.js
+++ src/route/web.js
@@ -1,14 +1,13 @@
 // nơi mà mỗi 1 khi truy cập vào đường link của website thì nó sẽ vào file này đầu tiên
 import express from 'express';
+import homeController from '../controllers/homeController'
 
 let router = express.Router();
 
 // app là 1 instant, 1 server là 1 ứng dụng, ta truyền ứng dụng vào bên trong server
 let initWebRoutes = (app) => {
     // rest api
-    router.get('/', (req, res) => {
-        return res.send("Hello world")
-    })
+    router.get('/', homeController.getHomePage)
     router.get('/about', (req, res) => {
         return res.send("How about today")
     })
