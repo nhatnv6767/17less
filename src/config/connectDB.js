62d647a3edbf01c18f823b05a82a1c80b0340549
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-14 23:09:16 +0900

    connect db

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,8 +2,19 @@
 const { Sequelize } = require('sequelize');
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('database', 'username', 'password', {
+const sequelize = new Sequelize('checkdata', 'root', null, {
     host: 'localhost',
     /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
     dialect: 'mysql'
 });
+
+let connectDB = () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+module.exports = connectDB;
