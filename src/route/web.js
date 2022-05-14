b20dedaf7584a674ee882d1ce76d6d7fa534caa5
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-14 14:31:51 +0900

    config 3

---
+++ src/route/web.js
@@ -0,0 +1,14 @@
+// nơi mà mỗi 1 khi truy cập vào đường link của website thì nó sẽ vào file này đầu tiên
+import exports from "express"
+
+let router = express.Router();
+
+// app là 1 instant, 1 server là 1 ứng dụng, ta truyền ứng dụng vào bên trong server
+let initWebRoutes = (app) => {
+    router.get('/', (req, res) => {
+        return res.send("Hello world")
+    })
+    return app.use("/", router);
+}
+
+module.exports = initWebRoutes;
