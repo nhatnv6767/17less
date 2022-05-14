71670241b7410919f8652bb2275a5017d24a4ff4
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-14 23:24:21 +0900

    connect db

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,13 @@
-let getHomePage = (req, res) => {
-    // vì đã cấu hình đường dẫn trong file viewEngine.js nên sẽ tự động tìm 
-    return res.render('homepage.ejs')
+import db from '../models/index'
+let getHomePage = async (req, res) => {
+    try {
+        let data = await db.user.findAll();
+        // vì đã cấu hình đường dẫn trong file viewEngine.js nên sẽ tự động tìm 
+        return res.render('homepage.ejs')
+    } catch (e) {
+        console.log(e)
+    }
+
 }
 
 module.exports = {
