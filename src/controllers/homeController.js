e7604128a989f0ba1db659ac4642626093d07314
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-14 23:30:03 +0900

    turn off logging

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,6 +2,7 @@ import db from '../models/index'
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
+        console.log(data)
         // vì đã cấu hình đường dẫn trong file viewEngine.js nên sẽ tự động tìm 
         return res.render('homepage.ejs')
     } catch (e) {
