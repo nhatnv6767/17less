e7521bcb7a6cdbb8a420c13a64a73cc8c8867309
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-15 03:39:14 +0900

    #23 CRUD bootstrap FORM action of form - Hash password 2

--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -1,11 +1,19 @@
+import bcrypt from 'bcryptjs';
+const salt = bcrypt.genSaltSync(10);
+
 let createNewUser = (data) => {
     console.log("Data from service", data);
 }
 
 let hashUserPassword = (password) => {
     // dùng promise để luôn luôn trả ra kết quả, tránh việc xử lý bất đồng bộ
-    return new Promise((resolve, reject) => {
-        
+    return new Promise(async (resolve, reject) => {
+        try {
+            var hash = await bcrypt.hashSync("B4c0/\/", salt);
+        } catch (e) {
+            reject(e);
+        }
+
     })
 }
 
