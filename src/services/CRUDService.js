746fd13d32dedcd41457b7260036a6753add8bd9
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-15 03:51:03 +0900

    #23 CRUD bootstrap FORM action of form - insert data

--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -1,9 +1,27 @@
 import bcrypt from 'bcryptjs';
+import db from "../models/index"
 const salt = bcrypt.genSaltSync(10);
 
 let createNewUser = async (data) => {
-    let hashPasswordFromBcrypt = await hashUserPassword(data.password)
-    console.log("Hash password", hashPasswordFromBcrypt);
+    return new Promise(async (resolve, reject) => {
+        try {
+            let hashPasswordFromBcrypt = await hashUserPassword(data.password)
+            // modelName của model user.js
+            await db.User.create({
+                email: data.email,
+                password: hashPasswordFromBcrypt,
+                firstName: data.firstName,
+                lastName: data.lastName,
+                address: data.address,
+                phonenumber: data.phonenumber,
+                gender: data.gender === '1' ? true : false,
+                roleId: data.roleId,
+            })
+            resolve('ok create a new user succeed')
+        } catch (e) {
+            reject(e);
+        }
+    })
 }
 
 let hashUserPassword = (password) => {
@@ -11,6 +29,7 @@ let hashUserPassword = (password) => {
     return new Promise(async (resolve, reject) => {
         try {
             var hashPassword = await bcrypt.hashSync(password, salt);
+            // tương tự return
             resolve(hashPassword)
         } catch (e) {
             reject(e);
