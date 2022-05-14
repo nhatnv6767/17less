9ff5d99607864fd12f1800f0ba3ef5fcef08b804
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-14 22:28:30 +0900

    tao faKE DU LIEU

--- src/seeders/20220514132511-demo-user.js
+++ src/seeders/20220514132511-demo-user.js
@@ -10,7 +10,16 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
+     * 
+     * SEE ON DOCUMENT: ->>>> https://sequelize.org/docs/v6/other-topics/migrations/
     */
+    return queryInterface.bulkInsert('Users', [{
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'example@example.com',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
   },
 
   down: async (queryInterface, Sequelize) => {
@@ -20,5 +29,12 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+    return queryInterface.bulkDelete('Users', null, {});
   }
 };
+
+/**
+ * up khi chay binh thuong, them du lieu vao
+ * down: khi cancel them du lieu rollback
+ * 
+ */
