dd4f4662598497febd7895d2311f53ebdaecd725
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-15 19:27:56 +0900

    #28 DELETE CRUD 3

--- src/services/CRUDService.js
+++ src/services/CRUDService.js
@@ -95,9 +95,23 @@ let updateUserData = (data) => {
     })
 }
 
+let deleteUserById = (userId) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let user = await db.User.findOne({
+                where: { id: userId }
+            })
+
+        } catch (e) {
+            reject(e);
+        }
+    })
+}
+
 module.exports = {
     createNewUser: createNewUser,
     getAllUser: getAllUser,
     getUserInfoById: getUserInfoById,
     updateUserData: updateUserData,
+    deleteUserById: deleteUserById,
 }
