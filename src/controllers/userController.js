dae8884eb6cfd014e65f609a49b9cffbe3d8b691
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-17 21:31:08 +0900

    #35 API Login 4 noi dung

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,14 @@
 let handleLogin = (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
+    // check email exist 
+    // compare password
+    // return userInfor
+    // access_token:JWT <json web token>
     return res.status(200).json({
         message: 'hello world',
         yourEmail: email,
-        test: 'test',
+
     })
 }
 
