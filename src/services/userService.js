4585e2b115a722a95d824de146f79f5ab360ec13
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-17 22:13:11 +0900

    #35 API Login 14

--- src/services/userService.js
+++ src/services/userService.js
@@ -16,7 +16,15 @@ let handleUserLogin = (email, password) => {
                 // giải thích bên dưới
                 if (user) {
                     // compare password
-                    // bcrypt.compareSync("not_bacon",hash); 
+                    let check = await bcrypt.compareSync(password, user.password);
+                    if (check) {
+                        userData.errCode = 0;
+                        userData.errMessage = 'Ok'
+                        userData.user = user;
+                    } else {
+                        userData.errCode = 3;
+                        userData.errMessage = 'Wrong password'
+                    }
                 } else {
                     userData.errCode = 2,
                         userData.errMessage = `User's not found`
@@ -36,15 +44,6 @@ let handleUserLogin = (email, password) => {
     })
 }
 
-let compareUserPassword = () => {
-    return new Promise(async (resolve, reject) => {
-        try {
-
-        } catch (e) {
-            reject(e);
-        }
-    })
-}
 
 let checkUserEmail = (userEmail) => {
     return new Promise(async (resolve, reject) => {
