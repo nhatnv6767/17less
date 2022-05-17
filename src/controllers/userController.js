5a8cb926028c4b79043e1d4aa806c1b11e0956c9
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-17 21:36:05 +0900

    #35 API Login 5

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,17 @@
 let handleLogin = (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
-    // check email exist 
-    // compare password
-    // return userInfor
-    // access_token:JWT <json web token>
+    if (!email || !password) {
+        return res.status(500).json({
+            errCode: 1,
+            message: 'Missing inputs parameter!'
+        })
+    }
+
+
+
     return res.status(200).json({
+        errCode: 0,
         message: 'hello world',
         yourEmail: email,
 
