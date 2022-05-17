464af627b11af70d475476f691225f6fe824d52a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-17 21:24:59 +0900

    #35 API Login 3

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,10 @@
 let handleLogin = (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
     return res.status(200).json({
-        message: 'hello world'
+        message: 'hello world',
+        yourEmail: email,
+        test: 'test',
     })
 }
 
