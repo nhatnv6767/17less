9186aa67697faa13954738cc6fdf70f675f05225
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-17 21:47:51 +0900

    #35 API Login 8

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,12 +3,18 @@ import db from '../models/index'
 let handleUserLogin = (email, password) => {
     return new Promise(async (resolve, reject) => {
         try {
+            let userData = {}
+
             let isExist = await checkUserEmail(email);
             if (isExist) {
                 // user already exists
                 // compare password
+                resolve()
             } else {
                 // return error
+                userData.errCode = 1;
+                userData.errMessage = `Your's Email isn't exist. Try again!`
+                resolve(userData);
             }
         } catch (e) {
             reject(e);
