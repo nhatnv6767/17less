ce9fdaac858be1d895ff465b0268e9ab098c5141
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-19 23:44:28 +0900

    #39 CRUD with ReactJS | API (NodeJS) 9

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -45,15 +45,21 @@ let handleGetAllUsers = async (req, res) => {
 
 let handleCreateNewUser = async (req, res) => {
     let message = await userService.createNewUser(req.body)
-    console.log(message)
+    return res.status(200).json(message)
+}
+
+let handleDeleteUser = async (req, res) => {
+    if (!req.body.id) {
+        return res.status(200).json({
+            errCode: 1,
+            errMessage: 'Missing required parameters'
+        })
+    }
+    let message = await userService.deleteUser(req.body.id)
     return res.status(200).json(message)
 }
 
 let handleEditUser = async (req, res) => {
-
-}
-
-let handleDeleteUser = async (req, res) => {
 
 }
 
