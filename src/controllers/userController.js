3ffaa655156247284d006b205f8cea99cf7a02c9
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-19 21:00:14 +0900

    #37 CRUD with ReactJS 2

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -24,7 +24,14 @@ let handleLogin = async (req, res) => {
 }
 
 let handleGetAllUsers = async (req, res) => {
-    let users = await userService.getAllUsers()
+    let id = req.body.id; // all, id
+    let users = await userService.getAllUsers(id)
+
+    return res.status(200).json({
+        errCode: 0,
+        errMessage: 'OK',
+        users
+    })
 }
 
 module.exports = {
