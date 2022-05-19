2d95dc490fe0627cd2bf47d4829313c2b113af39
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-19 23:31:08 +0900

    #39 CRUD with ReactJS | API (NodeJS) 7

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -46,7 +46,7 @@ let handleGetAllUsers = async (req, res) => {
 let handleCreateNewUser = async (req, res) => {
     let message = await userService.createNewUser(req.body)
     console.log(message)
-    return res.json(200).json(message)
+    return res.status(200).json(message)
 }
 
 module.exports = {
