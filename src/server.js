7a3774130f7811281d6f2de3e45a7c8225cf5711
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-23 21:29:01 +0900

    CORS

--- src/server.js
+++ src/server.js
@@ -1,35 +1,59 @@
 import express from "express";
 // lấy được các tham số từ client gửi lên
 import bodyParser from "body-parser";
-import viewEngine from "./config/viewEngine"
-import initWebRoutes from "./route/web"
-import connectDB from './config/connectDB'
-import cors from 'cors'
-require('dotenv').config();
-
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from "./route/web";
+import connectDB from "./config/connectDB";
+import cors from "cors";
+require("dotenv").config();
 
 // instance cua express
-let app = express()
+let app = express();
 
-app.use(cors({
-    origin: function (origin, callback) {
-        return callback(null, true);
-    },
-    optionsSuccessStatus: 200,
-    credentials: true
-}));
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         return callback(null, true);
+//     },
+//     optionsSuccessStatus: 200,
+//     credentials: true
+// }));
+
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 //config app
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-viewEngine(app)
-initWebRoutes(app)
+viewEngine(app);
+initWebRoutes(app);
 
 connectDB();
 
 let port = process.env.PORT;
 app.listen(port, () => {
-    //callback
-    console.log("Backend NodeJS is running on the port: " + port)
-})
+  //callback
+  console.log("Backend NodeJS is running on the port: " + port);
+});
