370cc1288977900a590f5e776a177e4bd9742ac7
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-28 19:27:45 +0900

    #52 import data from excel|| NODE JS first

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,74 +1,74 @@
-import userService from "../services/userService"
+import userService from "../services/userService";
 
 let handleLogin = async (req, res) => {
-    let email = req.body.email;
-    let password = req.body.password;
-    if (!email || !password) {
-        return res.status(500).json({
-            errCode: 1,
-            message: 'Missing inputs parameter!'
-        })
-    }
+  let email = req.body.email;
+  let password = req.body.password;
+  if (!email || !password) {
+    return res.status(500).json({
+      errCode: 1,
+      message: "Missing inputs parameter!",
+    });
+  }
 
-    let userData = await userService.handleUserLogin(email, password)
+  let userData = await userService.handleUserLogin(email, password);
 
+  return res.status(200).json({
+    errCode: userData.errCode,
+    message: userData.errMessage,
 
-    return res.status(200).json({
-        errCode: userData.errCode,
-        message: userData.errMessage,
-
-        // nếu mà có biến userData thì trả ra giá trị của nó, còn ko thì trả ra {}
-        user: userData.user ? userData.user : {}
-
-    })
-}
+    // nếu mà có biến userData thì trả ra giá trị của nó, còn ko thì trả ra {}
+    user: userData.user ? userData.user : {},
+  });
+};
 
 let handleGetAllUsers = async (req, res) => {
-    let id = req.query.id; // all, id
+  let id = req.query.id; // all, id
 
-    if (!id) {
-        return res.status(200).json({
-            errCode: 1,
-            errMessage: 'Missing required parameter',
-            users: []
-        })
-    }
-
-    let users = await userService.getAllUsers(id)
+  if (!id) {
     return res.status(200).json({
-        errCode: 0,
-        errMessage: 'OK',
-        users
-    })
+      errCode: 1,
+      errMessage: "Missing required parameter",
+      users: [],
+    });
+  }
 
-}
+  let users = await userService.getAllUsers(id);
+  return res.status(200).json({
+    errCode: 0,
+    errMessage: "OK",
+    users,
+  });
+};
 
 let handleCreateNewUser = async (req, res) => {
-    let message = await userService.createNewUser(req.body)
-    return res.status(200).json(message)
-}
+  let message = await userService.createNewUser(req.body);
+  return res.status(200).json(message);
+};
 
 let handleDeleteUser = async (req, res) => {
-    if (!req.body.id) {
-        return res.status(200).json({
-            errCode: 1,
-            errMessage: 'Missing required parameters'
-        })
-    }
-    let message = await userService.deleteUser(req.body.id)
-    return res.status(200).json(message)
-}
+  if (!req.body.id) {
+    return res.status(200).json({
+      errCode: 1,
+      errMessage: "Missing required parameters",
+    });
+  }
+  let message = await userService.deleteUser(req.body.id);
+  return res.status(200).json(message);
+};
 
 let handleEditUser = async (req, res) => {
-    let data = req.body;
-    let message = await userService.updateUserData(data)
-    return res.status(200).json(message)
-}
+  let data = req.body;
+  let message = await userService.updateUserData(data);
+  return res.status(200).json(message);
+};
+
+let getAllCode = async (req, res) => {};
 
 module.exports = {
-    handleLogin: handleLogin,
-    handleGetAllUsers: handleGetAllUsers,
-    handleCreateNewUser: handleCreateNewUser,
-    handleEditUser: handleEditUser,
-    handleDeleteUser: handleDeleteUser,
-}
+  handleLogin: handleLogin,
+  handleGetAllUsers: handleGetAllUsers,
+  handleCreateNewUser: handleCreateNewUser,
+  handleEditUser: handleEditUser,
+  handleDeleteUser: handleDeleteUser,
+  getAllCode: getAllCode,
+};
--- src/route/web.js
+++ src/route/web.js
@@ -1,31 +1,33 @@
 // nơi mà mỗi 1 khi truy cập vào đường link của website thì nó sẽ vào file này đầu tiên
-import express from 'express';
-import homeController from '../controllers/homeController'
-import userController from '../controllers/userController'
+import express from "express";
+import homeController from "../controllers/homeController";
+import userController from "../controllers/userController";
 
 let router = express.Router();
 
 // app là 1 instant, 1 server là 1 ứng dụng, ta truyền ứng dụng vào bên trong server
 let initWebRoutes = (app) => {
-    // rest api
-    router.get('/', homeController.getHomePage)
-    router.get('/about', (req, res) => {
-        return res.send("How about today")
-    })
-    router.get('/crud', homeController.getCRUD)
-    router.post('/post-crud', homeController.postCRUD)
-    router.get('/get-crud', homeController.displayGetCRUD);
-    router.get('/edit-crud', homeController.getEditCRUD);
-    router.post('/put-crud', homeController.putCRUD);
-    router.get('/delete-crud', homeController.deleteCRUD);
+  // rest api
+  router.get("/", homeController.getHomePage);
+  router.get("/about", (req, res) => {
+    return res.send("How about today");
+  });
+  router.get("/crud", homeController.getCRUD);
+  router.post("/post-crud", homeController.postCRUD);
+  router.get("/get-crud", homeController.displayGetCRUD);
+  router.get("/edit-crud", homeController.getEditCRUD);
+  router.post("/put-crud", homeController.putCRUD);
+  router.get("/delete-crud", homeController.deleteCRUD);
 
-    router.post('/api/login', userController.handleLogin);
-    router.get('/api/get-all-users', userController.handleGetAllUsers);
-    router.post('/api/create-new-user', userController.handleCreateNewUser)
-    router.put('/api/edit-user', userController.handleEditUser)
-    router.delete('/api/delete-user', userController.handleDeleteUser)
+  router.post("/api/login", userController.handleLogin);
+  router.get("/api/get-all-users", userController.handleGetAllUsers);
+  router.post("/api/create-new-user", userController.handleCreateNewUser);
+  router.put("/api/edit-user", userController.handleEditUser);
+  router.delete("/api/delete-user", userController.handleDeleteUser);
 
-    return app.use("/", router);
-}
+  router.get("/allcode", userController.getAllCode);
+
+  return app.use("/", router);
+};
 
 module.exports = initWebRoutes;
