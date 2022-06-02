75d31f87ff6a269698785b01c5c3e6d4ed2f23e1
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-03 00:06:04 +0900

    #62 Write, Read file image with sequelize 1 - edit modal NODEJS 1

--- src/migrations/migration-update-blob-user.js
+++ src/migrations/migration-update-blob-user.js
@@ -1,21 +1,33 @@
-//
-
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return Promise.all([
-      queryInterface.addColumn("tableName", "columnName1", {
-        type: Sequelize.STRING,
-      }),
-      queryInterface.addColumn("tableName", "columnName2", {
-        type: Sequelize.STRING,
-      }),
+      queryInterface.changeColumn(
+        "users",
+        "image",
+        {
+          type: Sequelize.BLOB,
+          allowNull: true,
+        },
+        {
+          transaction,
+        }
+      ),
     ]);
   },
 
   down: (queryInterface, Sequelize) => {
     return Promise.all([
-      queryInterface.removeColumn("tableName", "columnName1"),
-      queryInterface.removeColumn("tableName", "columnName2"),
+      queryInterface.changeColumn(
+        "users",
+        "image",
+        {
+          type: Sequelize.STRING,
+          allowNull: true,
+        },
+        {
+          transaction,
+        }
+      ),
     ]);
   },
 };
