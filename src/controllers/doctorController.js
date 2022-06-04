e215bcc825d6d70f2656892a09b504d1d8e0fd28
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-04 19:16:53 +0900

    #63 Load static doctors's infor NODEJS first

---
+++ src/controllers/doctorController.js
@@ -0,0 +1,9 @@
+import userService from "../services/userService";
+
+let getTopDoctorHome = async (req, res) => {
+  console.log("getTopDoctorHome");
+};
+
+module.exports = {
+  getTopDoctorHome: getTopDoctorHome,
+};
