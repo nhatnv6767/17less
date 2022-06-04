e6fc43733791e9b383f2d490025e6351481a33d8
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-04 19:45:58 +0900

    #63 Load static doctors's infor NODEJS first 8

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -4,7 +4,9 @@ let getTopDoctorHome = async (req, res) => {
   let limit = req.query.limit;
   if (!limit) limit = 10;
   try {
-    let doctors = await doctorService.getTopDoctorHome(limit);
+    let response = await doctorService.getTopDoctorHome(limit);
+
+    return res.status(200).json(response);
   } catch (e) {
     console.log(e);
     return res.status(200).json({
