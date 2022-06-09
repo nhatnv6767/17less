11222f348701ef2427b6fcf7ec95f11286443848
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-09 21:56:21 +0900

    #70 Section Details doctor <Server> 3 NODE JS API

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -63,19 +63,25 @@ let saveDetailInforDoctor = (inputData) => {
             if (
                 !inputData.doctorId ||
                 !inputData.contentHTML ||
-                !inputData.contentMarkdown
+                !inputData.contentMarkdown ||
+                !inputData.action
             ) {
                 resolve({
                     errCode: 1,
                     errMessage: "Missing parameter",
                 });
             } else {
-                await db.Markdown.create({
-                    contentHTML: inputData.contentHTML,
-                    contentMarkdown: inputData.contentMarkdown,
-                    description: inputData.description,
-                    doctorId: inputData.doctorId,
-                });
+                if(inputData.action === 'CREATE') {
+                    await db.Markdown.create({
+                        contentHTML: inputData.contentHTML,
+                        contentMarkdown: inputData.contentMarkdown,
+                        description: inputData.description,
+                        doctorId: inputData.doctorId,
+                    });
+                } else if(inputData.action === 'EDIT'){
+
+                }
+
                 resolve({
                     errCode: 0,
                     errMessage: "Save infor doctor succeed!",
