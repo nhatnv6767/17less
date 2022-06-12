e2468f6953cab046a0765bfcd62ae1a13cf1f655
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-12 20:45:37 +0900

    #80 Design display infor detail doctor 1 NODEJS fix API 4

--- src/models/allcode.js
+++ src/models/allcode.js
@@ -16,6 +16,11 @@ module.exports = (sequelize, DataTypes) => {
             });
             Allcode.hasMany(models.User, {foreignKey: "gender", as: "genderData"});
             Allcode.hasMany(models.Schedule, {foreignKey: "timeType", as: "timeTypeData"});
+
+            Allcode.hasMany(models.Doctor_Infor, {foreignKey: "priceId", as: "priceData"});
+            Allcode.hasMany(models.Doctor_Infor, {foreignKey: "provinceId", as: "provinceData"});
+            Allcode.hasMany(models.Doctor_Infor, {foreignKey: "paymentId", as: "paymentData"});
+
         }
     }
 
