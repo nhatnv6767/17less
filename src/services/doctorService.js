5dc02b4094612fe841f8dcf66f46ec71541f5840
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-14 00:26:20 +0900

    #83 Customize API to Load doctor's infor in Modal Booking 4 Go To NODE -> Create new API 4

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -373,16 +373,76 @@ let getExtraInforDoctorById = (idInput) => {
     });
 };
 
-let getProfileDoctorById = (doctorId) => {
+let getProfileDoctorById = (inputId) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (!doctorId) {
+            if (!inputId) {
                 resolve({
                     errCode: 1,
                     errMessage: "Missing required parameters"
                 });
             } else {
+                let data = await db.User.findOne({
+                    where: {id: inputId},
+                    attributes: {
+                        exclude: ["password"],
+                    },
+                    include: [
 
+                        {
+                            model: db.Allcode,
+                            as: "positionData",
+                            attributes: ["valueEn", "valueVi"],
+                        },
+                        {
+                            model: db.Allcode,
+                            as: "genderData",
+                            attributes: ["valueEn", "valueVi"],
+                        },
+
+                        {
+                            model: db.Doctor_Infor,
+                            attributes: {
+                                exclude: ["id", "doctorId"],
+                            },
+                            include: [
+                                {
+                                    model: db.Allcode,
+                                    as: "priceData",
+                                    attributes: ["valueEn", "valueVi"],
+                                },
+                                {
+                                    model: db.Allcode,
+                                    as: "provinceData",
+                                    attributes: ["valueEn", "valueVi"],
+                                },
+                                {
+                                    model: db.Allcode,
+                                    as: "paymentData",
+                                    attributes: ["valueEn", "valueVi"],
+                                },
+                            ]
+
+                        },
+
+                    ],
+                    raw: false,
+                    nest: true,
+                });
+
+                // convert image -> base64
+                if (data && data.image) {
+                    data.image = new Buffer(data.image, "base64").toString("binary");
+                }
+
+                if (!data) {
+                    data = {};
+                }
+
+                resolve({
+                    errCode: 0,
+                    data: data,
+                });
             }
         } catch (e) {
             reject(e);
