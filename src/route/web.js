0b6142c1658c7524a2be6995ac03b558f5339469
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-14 00:13:33 +0900

    #83 Customize API to Load doctor's infor in Modal Booking 4 Go To NODE -> Create new API 1

--- src/route/web.js
+++ src/route/web.js
@@ -38,7 +38,7 @@ let initWebRoutes = (app) => {
     router.get("/api/get-schedule-doctor-by-date", doctorController.getScheduleDoctorByDate);
 
     router.get("/api/get-extra-infor-doctor-by-id", doctorController.getExtraInforDoctorById);
-
+    router.get("/api/get-profile-doctor-by-id", doctorController.getProfileDoctorById);
 
     return app.use("/", router);
 };
