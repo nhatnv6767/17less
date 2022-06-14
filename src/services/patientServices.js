8db144fd8e8a492f59627ec4d327e666f159c767
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-15 00:53:26 +0900

    #84 Sequelize findOnCreacte 13 NODEJS NEW API 6

--- src/services/patientServices.js
+++ src/services/patientServices.js
@@ -1,10 +1,35 @@
 import db from "../models/index";
 import _ from "lodash";
 
-let postBookAppointment = () => {
+require('dotenv').config();
+
+let postBookAppointment = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
+            if (!data.email) {
+                resolve({
+                    errCode: 1,
+                    errMessage: "Missing required parameters"
+                });
+            } else {
+                // upsert patient
+                let user = await db.User.findOrCreate({
+                    where: {
+                        email: data.email
+                    },
+                    // if dont have email
+                    default: {
+                        email: data.email,
+                        roleId: 'R3'
+                    }
+                });
 
+                resolve({
+                    errCode: 0,
+                    errMessage: "OK",
+                    data: user,
+                });
+            }
 
         } catch (e) {
             reject(e);
