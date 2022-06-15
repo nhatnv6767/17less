7e997f1d36b668a62665ed8b7b92ffc1f71c215b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-16 02:49:29 +0900

    #87 Customize sending data 4 Go to NODE 5

--- src/migrations/migration-create-booking.js
+++ src/migrations/migration-create-booking.js
@@ -27,6 +27,10 @@ module.exports = {
                 type: Sequelize.STRING
             },
 
+            token: {
+                type: Sequelize.STRING
+            },
+
             createdAt: {
                 allowNull: false,
                 type: Sequelize.DATE
