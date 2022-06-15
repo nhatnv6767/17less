6bec750f4f496de8c8f83b330cbc46bfb54a9207
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-16 02:17:06 +0900

    #87 Customize sending data 3 NODE TESTING 4

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -307,6 +307,12 @@ let getScheduleDoctorByDate = (doctorId, date) => {
                             as: "timeTypeData",
                             attributes: ["valueEn", "valueVi"],
                         },
+
+                        {
+                            model: db.User,
+                            as: "doctorData",
+                            attributes: ["firstName", "lastName"],
+                        },
                     ],
                     raw: false,
                     nest: true,
