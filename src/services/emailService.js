e8992f1377d2140864032a255d1365fd082b004f
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-16 01:15:17 +0900

    #86 Config seding email confirm NODE JS 6

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,27 +1,27 @@
-require('dotenv').config()
-import nodemailer from 'nodemailer'
+require('dotenv').config();
+import nodemailer from 'nodemailer';
 
-let sendSimpleEmail = async () => {
+let sendSimpleEmail = async (receiverEmail) => {
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
         host: "smtp.ethereal.email",
         port: 587,
         secure: false, // true for 465, false for other ports
         auth: {
-            user: testAccount.user, // generated ethereal user
-            pass: testAccount.pass, // generated ethereal password
+            user: process.env.EMAIL_APP.user, // generated ethereal user
+            pass: process.env.EMAIL_APP_PASSWORD.pass, // generated ethereal password
         },
     });
 
     // send mail with defined transport object
     let info = await transporter.sendMail({
         from: '"Fred Foo 👻" <foo@example.com>', // sender address
-        to: "bar@example.com, baz@example.com", // list of receivers
+        to: receiverEmail, // list of receivers
         subject: "Hello ✔", // Subject line
         text: "Hello world?", // plain text body
         html: "<b>Hello world?</b>", // html body
     });
-}
+};
 
 
 // async..await is not allowed in global scope, must use a wrapper
@@ -32,4 +32,4 @@ async function main() {
 
 module.exports = {
     sendSimpleEmail: sendSimpleEmail
-}
+};
