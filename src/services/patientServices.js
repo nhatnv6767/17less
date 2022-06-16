a6080b3aed94d1068b44dca7b75fd74ff0f8c883
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-16 20:18:40 +0900

    #88 API Confirm Email/token NODEJS 1

--- src/services/patientServices.js
+++ src/services/patientServices.js
@@ -3,7 +3,17 @@ import _ from "lodash";
 
 require('dotenv').config();
 import emailService from "./emailService";
+import {v4 as uuidv4} from 'uuid';
 
+
+let buildUrlEmail = () => {
+    let result = "";
+    let id = uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+
+    result =
+        `${process.env.URL_REACT}/verify-booking?token=${id}&doctorId=${doctorId}&patientId=${patientId}`
+    return result;
+};
 let postBookAppointment = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
