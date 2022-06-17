2c8827d34fed26ba424575c221f1090183820169
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-18 01:14:58 +0900

    #90 Specialty func 18 NODE JS API 4

--- src/controllers/specialtyController.js
+++ src/controllers/specialtyController.js
@@ -1,7 +1,9 @@
+import specialtyService from "../services/specialtyService";
+
 let createSpecialty = async (req, res) => {
 
-}
+};
 
 module.exports = {
     createSpecialty: createSpecialty
-}
+};
