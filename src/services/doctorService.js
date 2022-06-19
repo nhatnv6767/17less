846ab3b2e4e4a00127233d06ca9254e34f6333fa
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-19 23:58:39 +0900

    #92 Details Specialty 12 NODE API 1

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -62,25 +62,35 @@ let getAllDoctors = () => {
     });
 };
 
+let checkRequiredFields = (inputData) => {
+    let arrFields = ["doctorId", "contentHTML", "contentMarkdown", "action", "selectedPrice",
+        "selectedPayment", "selectedProvince", "nameClinic", "addressClinic", "note", "specialtyId"
+    ]
+    let isValid = true;
+    let element = "";
+    for (let i = 0; i < arrFields.length; i++) {
+        if (!inputData[arrFields[i]]) {
+            isValid = false
+            element = arrFields[i]
+            break
+        }
+    }
+    return {
+        isValid: isvalid,
+        element: element,
+    }
+}
+
 let saveDetailInforDoctor = (inputData) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (
-                !inputData.doctorId ||
-                !inputData.contentHTML ||
-                !inputData.contentMarkdown ||
-                !inputData.action ||
 
-                !inputData.selectedPrice ||
-                !inputData.selectedPayment ||
-                !inputData.selectedProvince ||
-                !inputData.nameClinic ||
-                !inputData.addressClinic ||
-                !inputData.note
-            ) {
+            let checkObj = checkRequiredFields(inputData);
+
+            if (checkObj.isValid === false) {
                 resolve({
                     errCode: 1,
-                    errMessage: "Missing parameter",
+                    errMessage: `Missing parameter: ${checkObj.element}`,
                 });
             } else {
 
