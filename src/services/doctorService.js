7adbe79a49303ba4c4c12c5972df3d64164366db
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-20 00:03:10 +0900

    #92 Details Specialty 12 NODE API 2

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -65,21 +65,21 @@ let getAllDoctors = () => {
 let checkRequiredFields = (inputData) => {
     let arrFields = ["doctorId", "contentHTML", "contentMarkdown", "action", "selectedPrice",
         "selectedPayment", "selectedProvince", "nameClinic", "addressClinic", "note", "specialtyId"
-    ]
+    ];
     let isValid = true;
     let element = "";
     for (let i = 0; i < arrFields.length; i++) {
         if (!inputData[arrFields[i]]) {
-            isValid = false
-            element = arrFields[i]
-            break
+            isValid = false;
+            element = arrFields[i];
+            break;
         }
     }
     return {
         isValid: isvalid,
         element: element,
-    }
-}
+    };
+};
 
 let saveDetailInforDoctor = (inputData) => {
     return new Promise(async (resolve, reject) => {
@@ -138,6 +138,8 @@ let saveDetailInforDoctor = (inputData) => {
                     doctorInfor.nameClinic = inputData.nameClinic;
                     doctorInfor.addressClinic = inputData.addressClinic;
                     doctorInfor.note = inputData.note;
+                    doctorInfor.specialtyId = inputData.specialtyId;
+                    doctorInfor.clinicId = inputData.clinicId;
                     await doctorInfor.save();
                 } else {
                     // create
@@ -150,6 +152,8 @@ let saveDetailInforDoctor = (inputData) => {
                         nameClinic: inputData.nameClinic,
                         addressClinic: inputData.addressClinic,
                         note: inputData.note,
+                        specialtyId: inputData.specialtyId,
+                        clinicId: inputData.clinicId,
                     });
                 }
 
