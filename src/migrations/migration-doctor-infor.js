854523286561f5d07d5741aab1c56e6d6ee9af62
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-19 19:22:32 +0900

    #92 Details Specialty NODE JS 1

--- src/migrations/migration-doctor-infor.js
+++ src/migrations/migration-doctor-infor.js
@@ -12,6 +12,15 @@ module.exports = {
                 type: Sequelize.INTEGER,
                 allowNull: false,
             },
+
+            specialtyId: {
+                type: Sequelize.INTEGER,
+            },
+
+            clinicId: {
+                type: Sequelize.INTEGER,
+            },
+
             priceId: {
                 type: Sequelize.STRING,
                 allowNull: false,
