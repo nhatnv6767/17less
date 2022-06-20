346f3b0a9db1b01b5bb896fc09f429b73c55581f
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-21 00:18:24 +0900

    #94 Load static detail specialty 3 NODE API 1

--- src/route/web.js
+++ src/route/web.js
@@ -50,6 +50,7 @@ let initWebRoutes = (app) => {
     // For specialty
     router.post("/api/create-new-specialty", specialtyController.createSpecialty);
     router.get("/api/get-all-specialty", specialtyController.getAllSpecialty);
+    router.get("/api/get-detail-specialty-by-id", specialtyController.getDetailSpecialtyById);
 
     return app.use("/", router);
 };
