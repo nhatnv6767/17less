9feefec6b06fd7c2dc63cc693733d884f9b944f1
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-21 00:25:15 +0900

    #94 Load static detail specialty 3 NODE API 3

--- src/services/specialtyService.js
+++ src/services/specialtyService.js
@@ -50,7 +50,14 @@ let getAllSpecialty = () => {
 let getDetailSpecialtyById = (inputId) => {
     return new Promise(async (resolve, reject) => {
         try {
-            
+            if (!inputId) {
+                resolve({
+                    errCode: 1,
+                    errMessage: "Missing required parameters"
+                });
+            } else {
+                
+            }
         } catch (e) {
             reject(e);
         }
