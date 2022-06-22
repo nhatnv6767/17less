844600081d136d9bfd8a259539f54644ad05b9d5
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-23 01:15:36 +0900

    #97 Detail Clinic 1 NODE JS API 1

--- src/controllers/clinicController.js
+++ src/controllers/clinicController.js
@@ -12,6 +12,33 @@ let createClinic = async (req, res) => {
     }
 };
 
+let getAllClinic = async (req, res) => {
+    try {
+        let infor = await clinicService.getAllClinic();
+        return res.status(200).json(infor);
+    } catch (e) {
+        console.log(e);
+        return res
+            .status(200)
+            .json({errCode: -1, errMessage: "Error from server"});
+    }
+};
+
+let getDetailClinicById = async (req, res) => {
+    try {
+        let infor = await clinicService.getAllClinic(req.query.id, req.query.location);
+        return res.status(200).json(infor);
+    } catch (e) {
+        console.log(e);
+        return res
+            .status(200)
+            .json({errCode: -1, errMessage: "Error from server"});
+    }
+};
+
+
 module.exports = {
     createClinic: createClinic,
+    getAllClinic: getAllClinic,
+    getDetailClinicById: getDetailClinicById,
 };
--- src/route/web.js
+++ src/route/web.js
@@ -5,7 +5,7 @@ import userController from "../controllers/userController";
 import doctorController from "../controllers/doctorController";
 import patientController from "../controllers/patientController";
 import specialtyController from "../controllers/specialtyController";
-import clinicController from "../controllers/clinicController"
+import clinicController from "../controllers/clinicController";
 
 let router = express.Router();
 
@@ -56,8 +56,8 @@ let initWebRoutes = (app) => {
     // For clinic
 
     router.post("/api/create-new-clinic", clinicController.createClinic);
-    // router.get("/api/get-all-specialty", specialtyController.getAllSpecialty);
-    // router.get("/api/get-detail-specialty-by-id", specialtyController.getDetailSpecialtyById);
+    router.get("/api/get-all-clinic", clinicController.getAllClinic);
+    router.get("/api/get-detail-clinic-by-id", clinicController.getDetailClinicById);
 
     return app.use("/", router);
 };
