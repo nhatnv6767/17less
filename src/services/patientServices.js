9c31377182a33282012e2d3f1f358fe177c7bdd8
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-24 01:22:09 +0900

    #98 Medical appointment list management interface 14 NODEJS API 5

--- src/services/patientServices.js
+++ src/services/patientServices.js
@@ -15,7 +15,15 @@ let buildUrlEmail = (doctorId, token) => {
 let postBookAppointment = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (!data.email || !data.doctorId || !data.timeType || !data.date || !data.fullName) {
+            if (
+                !data.email ||
+                !data.doctorId ||
+                !data.timeType ||
+                !data.date ||
+                !data.fullName ||
+                !data.selectedGender ||
+                !data.address
+            ) {
                 resolve({
                     errCode: 1,
                     errMessage: "Missing required parameters"
@@ -40,7 +48,9 @@ let postBookAppointment = (data) => {
                     // if dont have email
                     defaults: {
                         email: data.email,
-                        roleId: 'R3'
+                        roleId: 'R3',
+                        gender: data.selectedGender,
+                        address: data.address,
                     },
                 });
 
