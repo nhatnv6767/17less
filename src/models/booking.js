596d21ecd876971bae9a28cdc610b74f577f9b58
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-24 01:47:26 +0900

    #98 Medical appointment list management interface 14 NODEJS API 9

--- src/models/booking.js
+++ src/models/booking.js
@@ -11,6 +11,13 @@ module.exports = (sequelize, DataTypes) => {
          */
         static associate(models) {
             // define association here
+
+            Schedule.belongsTo(models.User, {
+                    foreignKey: "patientId",
+                    targetKey: "id",
+                    as: "patientData",
+                }
+            );
         }
     };
     Booking.init({
