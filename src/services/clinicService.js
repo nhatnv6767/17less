503604ff99097a40d442aa02cedcb9f38f268a3e
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-23 21:45:28 +0900

    #97 Detail Clinic 17 NODEJS API 1

--- src/services/clinicService.js
+++ src/services/clinicService.js
@@ -49,10 +49,42 @@ let getAllClinic = () => {
     });
 };
 
-let getDetailClinicById = () => {
+let getDetailClinicById = (inputId) => {
     return new Promise(async (resolve, reject) => {
         try {
 
+            if (!inputId) {
+                resolve({
+                    errCode: 1,
+                    errMessage: "Missing required parameters"
+                });
+            } else {
+                let data = await db.Clinic.findOne({
+                    where: {
+                        id: inputId
+                    },
+                    attributes: ["descriptionHTML", "descriptionMarkdown"],
+                });
+
+                if (data) {
+                    let doctorClinic = [];
+
+                    doctorClinic = await db.Doctor_Infor.findAll({
+                        where: {clinicId: inputId},
+                        attributes: ["doctorId", "provinceId"]
+                    });
+
+                    data.doctorClinic = doctorClinic;
+                } else {
+                    data = {};
+                }
+
+                resolve({
+                    errCode: 0,
+                    errMessage: "Ok",
+                    data
+                });
+            }
         } catch (e) {
             reject(e);
         }
