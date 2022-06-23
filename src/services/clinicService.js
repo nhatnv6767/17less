0b668bf712d21038ce0e825eed708a3668b0d7dc
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-23 21:02:46 +0900

    #97 Detail Clinic 1 NODE JS API 4

--- src/services/clinicService.js
+++ src/services/clinicService.js
@@ -31,7 +31,18 @@ let createClinic = (data) => {
 let getAllClinic = () => {
     return new Promise(async (resolve, reject) => {
         try {
-
+            let data = await db.Clinic.findAll();
+            if (data && data.length > 0) {
+                data.map(item => {
+                    item.image = new Buffer(item.image, "base64").toString("binary");
+                    return item;
+                });
+            }
+            resolve({
+                errCode: 0,
+                errMessage: "OK",
+                data,
+            });
         } catch (e) {
             reject(e);
         }
