a88bc2935d6402b6d0ec1f7b80573083d4ba411a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-24 22:21:31 +0900

    #99 Show list patients for doctor 1 NODEJS  3

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -504,6 +504,12 @@ let getListPatientForDoctor = (doctorId, date) => {
                                 },
                             ]
                         },
+
+                        {
+                            model: db.Allcode,
+                            as: "timeTypeDataPatient",
+                            attributes: ["valueEn", "valueVi"],
+                        }
                     ],
                     raw: false,
                     nest: true,
