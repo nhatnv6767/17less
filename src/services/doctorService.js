eb1056268819902035c72b508f17b378f8fb2ea2
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-25 21:46:13 +0900

    #100 Manage Patients bonus func  38 NODEJS 10

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -530,7 +530,12 @@ let getListPatientForDoctor = (doctorId, date) => {
 let sendRemedy = (data) => {
     return new Promise(async (resolve, reject) => {
         try {
-            if (!data.email || !data.doctorId || !data.patientId || !data.timeType) {
+            if (!data.email ||
+                !data.doctorId ||
+                !data.patientId ||
+                !data.timeType ||
+                !data.imgBase64
+            ) {
                 resolve({
                     errCode: 1,
                     errMessage: "Missing required parameters!!"
