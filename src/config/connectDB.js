4c549dd88c197b32bfc2d07a86b234b9b3c75226
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-02 19:50:33 +0900

    #103 Deploy to Mainnet 5

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,12 +2,17 @@
 const { Sequelize } = require('sequelize');
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('d63m8huri282f9', 'dybqwefxtfguek', '0edc2facb04ae0552289a91d9a479a9bab9522145284fadcc47e9915f11910d7', {
-    host: 'ec2-23-23-151-191.compute-1.amazonaws.com',
-    /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
-    dialect: 'postgres',
-    logging: false,
-});
+const sequelize = new Sequelize(
+    'd63m8huri282f9', // database
+    'dybqwefxtfguek', // username
+    '0edc2facb04ae0552289a91d9a479a9bab9522145284fadcc47e9915f11910d7',
+    {
+        host: 'ec2-23-23-151-191.compute-1.amazonaws.com',
+        /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
+        dialect: 'postgres',
+        logging: false,
+    }
+);
 
 let connectDB = async () => {
     try {
