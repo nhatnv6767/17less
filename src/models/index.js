2438edb849ed00bfac2aa19e8cca026dd5f184e8
nhatnv6767 <nhatnvse90183@gmail.com>
2022-07-03 13:44:38 +0900

    #105 Deploy to heroku 5

--- src/models/index.js
+++ src/models/index.js
@@ -5,8 +5,6 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-// const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
@@ -28,13 +26,6 @@ const customizeConfig = {
   }
 }
 sequelize = new Sequelize(db_name, db_username, db_password, customizeConfig);
-
-
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
 
 fs
   .readdirSync(__dirname)
